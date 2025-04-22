88ecbc22cd1118f17701b15267df42bc4d246e4e
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-04-22 21:58:16 +0500

    9-Homework

---
+++ index.js
@@ -0,0 +1,23 @@
+let student = {
+    "first Name": "Eshmat",
+    lastName: "Toshmatov",
+    age: 20,
+    course: "Frontend"
+  };
+  
+  console.log(`${student["first Name"]} ${student.lastName} ${student.age}`);
+  
+
+
+//   part 2
+
+
+let data = {
+    name: "Phone",
+    model: "iPhone",
+    price: 700,
+    quantity: 8
+  };
+  
+  let umumiynarx = data.price * data.quantity;
+  console.log("Umumiy narx:", umumiynarx);
